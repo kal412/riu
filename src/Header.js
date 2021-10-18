028b929c6dba755bb9ee80668e1908ad22123065
kal412 <devkota.kalyan98@gmail.com>
2021-10-18 22:24:47 +0545

    wip: initial template

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="header_logo" src={"./images/logo.png"} alt="logo" />
+
+      <div className="header__nav">
+        <div className="header__option">Home</div>
+        <div className="header__option">Projects</div>
+        <div className="header__option">News</div>
+        <div className="header__option">Events</div>
+        <div className="header__option">Blog</div>
+        <div className="header__option">Vacancy</div>
+        <div className="header__option">About Us</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
