a46908bc3dd71a640da6f0db0422e20c929bd735
rai-aashish <2aashishrai@gmail.com>
2021-10-23 10:25:15 +0545

    wip: blog content page

--- src/components/Blog/Author.js
+++ src/components/Blog/Author.js
@@ -1,12 +1,26 @@
+
+import React, { useEffect,useState } from "react";
+import api from '../../axios'
+
 const Author = () => {
+    const [authorDetails,setAuthorDetails] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await api.get( `users/1`)
+            setAuthorDetails(response.data)
+        }
+        
+        fetchData()
+    }, [])
 
     return (
         <div className="author">
             <div className="author__dp">
-            <img src={"./images/logo-small.png"} alt="author"/>
+            <img src={authorDetails!=null? authorDetails.avatar_urls[96] : "Loading.."} alt="author"/>
             </div>
             <div className="author__details">
-            Author: ram shyam
+            Author: {authorDetails!=null? authorDetails.name : "Loading.."}
             <br />
             <small>Date: Oct-19 2021</small>
             </div>
