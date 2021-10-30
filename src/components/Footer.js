17972d10400d4d351164969606fe36118d5db243
kal412 <devkota.kalyan98@gmail.com>
2021-10-30 07:14:43 +0545

    wip: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import EmailIcon from "@mui/icons-material/Email";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GoogleIcon from "@mui/icons-material/Google";
-import "../styles/footer.css"
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
@@ -21,64 +21,90 @@ const Footer = () => {
             />
           </Link>
 
-          <div className="footer-location">
-            <p>
-              <LocationOnIcon /> <span>Kupondole,Lalitpur,Nepal</span>
-            </p>
-          </div>
+          <p>
+            <LocationOnIcon /> <span>Kupondole,Lalitpur,Nepal</span>
+          </p>
 
-          <div className="footer-call">
-            <p>
-              <CallIcon /> <span>+987456123 / 01-123456</span>
-            </p>
-          </div>
+          <p>
+            <CallIcon /> <span>+987456123 / 01-123456</span>
+          </p>
 
-          <div className="footer-mail">
-            <p>
-              <EmailIcon /> <span>riu@acem.org.np</span>
-            </p>
-          </div>
+          <p>
+            <EmailIcon /> <span>riu@acem.edu.np</span>
+          </p>
         </div>
 
-        <div className="footer-about">
+        <div>
           <h4>About RIU</h4>
-          <Link to="#">Lorem Ipsum</Link>
-          <Link to="#">Lorem Ipsum</Link>
-          <Link to="#">Lorem Ipsum</Link>
+          <ul>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+          </ul>
         </div>
 
-        <div className="footer-projects">
+        <div>
           <h4>Projects</h4>
-          <Link to="#">Lorem Ipsum</Link>
-          <Link to="#">Lorem Ipsum</Link>
+          <ul>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+          </ul>
         </div>
 
-        <div className="footer-news">
+        <div>
           <h4>News</h4>
-          <Link to="#">Lorem Ipsum</Link>
-          <Link to="#">Lorem Ipsum</Link>
-          <Link to="#">Lorem Ipsum</Link>
+          <ul>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+          </ul>
         </div>
 
-        <div className="footer-links">
+        <div>
           <h4>Quick Links</h4>
-          <Link to="#">Lorem Ipsum</Link>
-          <Link to="#">Lorem Ipsum</Link>
+          <ul>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+            <li>
+              <Link to="#">Lorem Ipsum</Link>
+            </li>
+          </ul>
         </div>
       </div>
 
       <div className="footer-bottom">
         <div className="copyright">
-          <p>
-            riu.org.np | &copy; Research and Innovation Unit, All Rights
-            Reserved
-          </p>
+          <p>riu.acem.edu.np | &copy; 2021 pixelbyte, All Rights Reserved</p>
         </div>
 
         <div className="footer-icons">
-          <FacebookIcon />
-          <TwitterIcon />
-          <GoogleIcon />
+          <a href="#facebook">
+            <FacebookIcon fontSize="large" />
+          </a>
+          <a href="#twitter">
+            {" "}
+            <TwitterIcon fontSize="large" />
+          </a>
+          <a href="#maps">
+            <GoogleIcon fontSize="large" />
+          </a>
         </div>
       </div>
     </div>
